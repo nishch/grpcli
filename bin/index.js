2338b5070963b860b2a2eba82945f19f59c0cfd6
Nishant Chaturvedi <nishant4263@gmail.com>
2018-09-25 13:59:48 +0530

    update the help text

--- bin/index.js
+++ bin/index.js
@@ -5,15 +5,15 @@ const master = require("../lib/master");
 
 program
     .version(require("../package.json").version, "-v, --version")
-    .option("-e, --endpoint <endpoint>", "service endpoint with host:port syntax")
-    .option("-s, --service <service>", "name of the gRPC service including package name")
-    .option("-m, --method <method>", "name of the method gRPC service method")
-    .option("-p, --protoFile <protoFile>", "protocol buffer file which has the service methods defined")
-    .option("-d, --dataFile [dataFile]", "path of the json file which has the request payload defined")
-    .option("-n, --number [number]", "number of times request should be made", 1)
-    .option("-w, --wait [wait]", "minimum wait time between 2 requests in milliseconds", 0)
+    .option("-e, --endpoint <endpoint>", "service endpoint in <host>:<port> format")
+    .option("-s, --service <service>", "fully qualified name of the gRPC service in <package name>.<service name> format")
+    .option("-m, --method <method>", "method name of the gRPC service")
+    .option("-p, --protoFile <protoFile>", "protocol buffer file which has the service definition")
+    .option("-d, --dataFile [dataFile]", "(optional) path of the json file which has the request payload defined")
+    .option("-n, --number [number]", "(optional) number of times request should be made", 1)
+    .option("-w, --wait [wait]", "(optional) minimum wait time between 2 requests in milliseconds", 0)
     //.option("-t, --timeout [timeout]", "max number of milliseconds to wait for the response")
-    .option("-r, --protoRoot [protoRoot]", "comma separated root directories where protocol buffer files are present")
+    .option("-r, --protoRoot [protoRoot]", "(optional)comma separated directories where protocol buffer files are present")
     .parse(process.argv);
 
 let { service, endpoint, method, dataFile, protoFile, number, wait, timeout, protoRoot } = program;
